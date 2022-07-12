5d1c0d918d20aa8068834f58bfdef8fca88f195d
dgetia <dgetia@DEV0013>
2022-07-12 12:22:00 +0400

    new

--- CurrencyAPI/Form1.b1f.cs
+++ CurrencyAPI/Form1.b1f.cs
@@ -1,6 +1,7 @@
 ﻿using SAPbouiCOM.Framework;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Xml;
 
 namespace CurrencyAPI
@@ -17,16 +18,12 @@ namespace CurrencyAPI
         /// </summary>
         public override void OnInitializeComponent()
         {
-            this.OptionBtn0.ClickBefore += new SAPbouiCOM._IOptionBtnEvents_ClickBeforeEventHandler(this.OptionBtn0_ClickBefore);
-            this.OptionBtn1 = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_4").Specific));
-            //  this.OptionBtn1.ClickAfter += new SAPbouiCOM._IButtonEvents_ClickAfterEventHandler(this.OptionBtn1_ClickAfter);
-            this.OptionBtn2 = ((SAPbouiCOM.OptionBtn)(this.GetItem("Item_5").Specific));
-            // this.OptionBtn2.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.OptionBtn2_ClickBefore);
-            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_6").Specific));
-            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("Item_7").Specific));
-            this.Button1.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button1_ClickBefore);
-            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_0").Specific));
-            this.Matrix1 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_2").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_4").Specific));
+            this.Button0.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button0_ClickBefore);
+            //  this.Button0.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button0_ClickBefore);
+            this.Button0.ClickAfter += new SAPbouiCOM._IButtonEvents_ClickAfterEventHandler(this.Button0_ClickAfter);
+            this.Button2 = ((SAPbouiCOM.Button)(this.GetItem("Item_5").Specific));
+            this.Button2.ClickBefore += new SAPbouiCOM._IButtonEvents_ClickBeforeEventHandler(this.Button2_ClickBefore);
             this.OnCustomInitialize();
 
         }
@@ -36,26 +33,45 @@ namespace CurrencyAPI
         /// </summary>
         public override void OnInitializeFormEvents()
         {
-            this.LoadAfter += new LoadAfterHandler(this.Form_LoadAfter);
 
         }
 
+        private SAPbouiCOM.Button Button2;
+        private SAPbouiCOM.Matrix Matrix0;
         private void OnCustomInitialize()
         {
+            //var company = (SAPbobsCOM.Company)SAPbouiCOM.Framework.Application.SBO_Application.Company.GetDICompany();
+            var comp = (SAPbobsCOM.Company)SAPbouiCOM.Framework.Application.SBO_Application.Company.GetDICompany();
+            var rs = (SAPbobsCOM.Recordset)comp.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+            rs.DoQuery("SELECT * FROM OCRN");
+            var ds = UIAPIRawForm.DataSources.DataTables.Item("DT_0");
+            ds.Rows.Add(rs.RecordCount);
+            int count = 0;
+            while (!rs.EoF)
+            {
+                var code = (string)rs.Fields.Item(0).Value;
+                ds.SetValue("Check", count, "N");
+                ds.SetValue("Currency", count++, code);
+                rs.MoveNext();
+            }
+            Matrix0.LoadFromDataSource();
 
         }
-        private SAPbouiCOM.OptionBtn OptionBtn1;
-        private SAPbouiCOM.OptionBtn OptionBtn2;
+       
+        private void Button2_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            var ds = UIAPIRawForm.DataSources.UserDataSources.Item("UD_0");
+            var date = DateTime.ParseExact(ds.Value, "yyyyMMdd", CultureInfo.InvariantCulture);
+        }
 
         private void OptionBtn0_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
         {
             BubbleEvent = true;
-            throw new System.NotImplementedException();
-
+          
         }
-
-        private SAPbouiCOM.Matrix Matrix0;
-        private SAPbouiCOM.Button Button1;
+        private SAPbouiCOM.OptionBtn OptionBtn1;
+        private SAPbouiCOM.OptionBtn OptionBtn2;
 
         private void Button1_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
         {
@@ -66,19 +82,37 @@ namespace CurrencyAPI
 
         private void OptionBtn1_ClickAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
         {
-           //logika gasaweri ukana pageze dasabrunebeli
-        }
-
-        private void OptionBtn2_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
-        {
-            BubbleEvent = true;
             
-
         }
+
+      
 
         private SAPbouiCOM.EditText EditText0;
         private SAPbouiCOM.Matrix Matrix1;
 
-        
+        private void OptionBtn2_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            var ds = UIAPIRawForm.DataSources.UserDataSources.Item("UD_0");
+            var date = DateTime.ParseExact(ds.Value, "yyyyMMdd", CultureInfo.InvariantCulture);
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void Button0_ClickAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        {
+            //Exchange_Rates_and_Indexes exch = new Exchange_Rates_and_Indexes();
+            //exch.show
+        }
+
+        private void Button0_ClickBefore(object sboObject, SAPbouiCOM.SBOItemEventArg pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            throw new System.NotImplementedException();
+
+        }
+
+        //private SAPbouiCOM.Button Button2;
+
     }
 }
